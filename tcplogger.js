273babe7d674adb81f53abb42e6bc77d5d616ab7
Tobias Abarbanell <tobias@abarbanell.de>
2016-07-31 19:25:22 +0000

    timestamps

--- tcplogger.js
+++ tcplogger.js
@@ -1,18 +1,23 @@
 // tcplogger - log any tcp connection to console
 
 var net = require('net');
+var log = function(s) {
+	var d = new Date();
+	console.log(d.toISOString() + ' - ' + s);
+};
+
 var server = net.createServer(function(socket) {
 	var client = socket.remoteAddress;
-	console.log("Connection from "+ client);
+	log("Connection from "+ client);
 	socket.on('data', function(data) { 
-		console.log(data);
+		log('STR:  ' + data.toString());
 	});
 	socket.on('end', function(end) {
-		console.log("Connection closed by " + client);
+		log("Connection closed by " + client);
 	});
 });
 
 server.listen(7000);
 
-console.log("TCP logger listening on port 7000");
+log("TCP logger listening on port 7000");
 
